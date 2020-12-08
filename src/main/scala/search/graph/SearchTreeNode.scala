116c98a245be59c505f23bf95056428bac332dcb
John Maraist <from-github@maraist.org>
2020-12-08 16:04:38 -0600

    Add file headers

--- src/main/scala/search/graph/SearchTreeNode.scala
+++ src/main/scala/search/graph/SearchTreeNode.scala
@@ -1,3 +1,13 @@
+// Copyright (C) 2020 John Maraist
+// See the LICENCE.txt file distributed with this work for additional
+// information regarding copyright ownership.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied, for NON-COMMERCIAL use.  See the License for the specific
+// language governing permissions and limitations under the License.
+
 package org.maraist.search.graph
 
 trait SearchTreeNode[Self <: SearchTreeNode[Self,State], State] {
